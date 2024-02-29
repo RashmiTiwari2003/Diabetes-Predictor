7a56fe81883304fa01c00d103224365e669a0961
Rashmi <rashmitiwari35832@gmail.com>
2024-02-29 23:28:51 +0530

    Model done

---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+with open('model.pkl','rb') as file:
+    clf = pickle.load(file)
+
+app = Flask(__name__)
+CORS(app) 
+
+@app.route('/back', methods=['POST'])
+def example_route():
+    X=[]
+    request_data = request.get_json()
+    print("Request JSON Object:", request_data)
+
+    for x in request_data:
+        data=float(request_data[x])
+        X.append(data)
+
+    # 1,155,78,31,0,25.3,0.6777,39
+    prediction = clf.predict([X])
+    print(prediction)
+
+    message="Diabetic" if prediction else "Non-Diabetic"
+
+    return jsonify({"message": message}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ script.js
@@ -0,0 +1,37 @@
+async function handleSubmit(e)
+{
+    e.preventDefault();
+    console.log(e.target.preg.value)
+    console.log(e.target.glucose.value)
+    console.log(e.target.bp.value)
+    console.log(e.target.skinthick.value)
+    console.log(e.target.insulin.value)
+    console.log(e.target.bmi.value)
+    console.log(e.target.pedifunc.value)
+    console.log(e.target.age.value)
+
+    const ob = {Pregnancies: e.target.preg.value,
+        Glucose: e.target.glucose.value,
+        BP: e.target.bp.value,
+        Skinthickness: e.target.skinthick.value,
+        Insulin: e.target.insulin.value,
+        BMI: e.target.bmi.value,
+        pedi: e.target.pedifunc.value,
+        Age: e.target.age.value
+    }
+
+    const resp = await fetch('http://127.0.0.1:5000/back', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(ob)
+    })
+    const res = await resp.json()
+    console.log(res.message)
+
+    const head = document.querySelector('#print')
+    head.innerText ="Patient is: " + res.message
+
+    document.querySelector('#form').reset();
+}
